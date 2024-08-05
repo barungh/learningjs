198ca2022947ee53d1a8a27bf27ce5f56b3e9697
iamninjax <aloneninjax@hotmail.com>
2024-08-05 22:47:01 +0530

    js object by Barun Sir 😎

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 function greet(name) {
   console.log(`Hey ${name}`);
 }
+
+let person = {
+
+  //this is a property of object person
+  name: "ninja",
+
+  //this is another property
+  married: false,
+
+  //this is a method
+  sing: function () {
+    console.log("I am singing a song");
+  },
+
+  //this is another method
+  hugMeNicely: function () {
+    console.log("😊")
+  },
+
+  //a weirdly named method
+  killMeByPressingMyNeck: function () {
+    console.log("😁😁😁")
+  }
+
+}
+
