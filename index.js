1cb0e07206ae51083ee0f2cfc52bfcbe08f0e80e
sutapaaditya <sutapaaditya84@gmail.com>
2024-08-05 22:50:32 +0530

    add in file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hey");
+console.log("good evening");
+let a = document.getElementById("btn");
+console.log(a);
+
+let user =[
+  {
+    "name": 'John',
+    "age": 18,
+  }
+]
+console.log(user);
